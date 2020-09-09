f274f89b2a7dce8d28228df718b7d7c7ddd67602
jmrittershaus <jmrittershaus@live.de>
2020-09-09 07:43:21 +0200

    Initial commit

---
+++ win-gui/stdafx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp : Quelldatei, die nur die Standard-Includes einbindet.
+// win-gui.pch ist der vorkompilierte Header.
+// stdafx.obj enthält die vorkompilierten Typinformationen.
+
+#include "stdafx.h"
+
+
